d5a98e8f8bebf7b82cd60547b82c5cb87725db8a
KhoiTraVinh <58519157+KhoiTraVinh@users.noreply.github.com>
2022-03-07 22:49:57 +0700

    test

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import express from 'express';
 
 const api = express();
 
+api.get("", () => {
+    res.send("KhoiDepTrai");
+})
+
 api.listen(5000, () => {
     console.log("Server run on 5000");
 })
