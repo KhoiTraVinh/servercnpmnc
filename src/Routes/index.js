3cb1de7be7b2ba2b6afa22a607a6d2a0c0119156
KhoiTraVinh <58519157+KhoiTraVinh@users.noreply.github.com>
2022-04-01 12:15:03 +0700

    update3

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 export const Routes = express();
-import {UserRouter} from './UserRouter.js';
+import {UserRouter} from './userRouter.js';
 
 
 Routes.use('/user', UserRouter);
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,4 @@
-import User from '../Models/User.js';
+import User from '../models/user.js';
 import expressAsyncHandler from 'express-async-handler';
 import {Token} from '../middleware/JWT.js';
 import bcrypt from 'bcryptjs';
